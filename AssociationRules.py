e0ae9341274bd45c57b8c5a4cc798d1f7879dfcd
周洋 <zhouyang081@ke.com>
2020-05-20 14:11:18 +0800

    算法修改

--- AssociationRules.py
+++ AssociationRules.py
@@ -1,136 +1,247 @@
 #!/usr/bin/env python3
+import json
+
 import pandas as pd
-import math
-def loadDataSet():
-    clean_data = pd.DataFrame(pd.read_csv("D:/result.csv"))
-    new_data = []
-    # 关联规则挖掘数据处理
-    for v in range(1, len(clean_data)):
-        item = []
-        item.append("category_id" + '='+ str(clean_data.loc[v,'category_id']))
-        item.append("comments_disabled" + '='+ str(clean_data.loc[v,'comments_disabled']))
-        item.append("ratings_disabled" + '='+ str(clean_data.loc[v,'ratings_disabled']))
-        item.append("video_error_or_removed" + '='+ str(clean_data.loc[v,'video_error_or_removed']))
-        #print(item)
-        new_data.append(item)
-    print(len(new_data))
-    return new_data # [[1, 3, 4], [2, 3, 5], [1, 2, 3, 5], [2, 5]]
-def createC1(dataSet):  # 产生单个item的集合
-    C1 = []
-    for transaction in dataSet:
-        for item in transaction:
-            if not [item] in C1:
-                C1.append([item])
+from progressbar import *
 
-    C1.sort()
-    return map(frozenset, C1)  # 给C1.list每个元素执行函数
+support = 0.1
+confidence = 0.5
+
+PropertyList = ['Priority', 'Location', 'Area Id', 'beat', 'Priority', 'Incident Type Id', 'Incident Type Descripe',
+                 'Event Number']
 
 
-def scanD(D, ck, minSupport):  # dataset,a list of candidate set,最小支持率 支持度计数
+class AssociationRules():
+    def __init__(self):
+        self.support = support
+        self.confidence = confidence
 
-    ssCnt = {}
-    # temp_D = list(D)
-    numItem = float(len(D))
-    temp_ck = list(ck)
-    for tid in D:
-        for can in temp_ck:
-            if can.issubset(tid):
-                if can not in ssCnt:
-                    ssCnt[can] = 1
+    # Apriori算法
+    def apriori(self, dataSet):
+        # 生成单元数候选项集
+        cell = self.cellGeneration(dataSet)
+        dataSet = [set(data) for data in dataSet]
+        F1, supRata = self.CkLowSupportFilter(dataSet, cell)
+        F = [F1]
+        k = 2
+        while len(F[k - 2]) > 0:
+            # 大于2时，合并检测
+            Ck = self.aprioriGeneration(F[k - 2], k)
+            Fk, support_k = self.CkLowSupportFilter(dataSet, Ck)
+            supRata.update(support_k)
+            F.append(Fk)
+            k += 1
+        return F, supRata
+
+    # 生成单元数候选项集
+    def cellGeneration(self, dataSet):
+        cell = []
+        progress = ProgressBar()
+        for data in progress(dataSet):
+            for item in data:
+                if [item] not in cell:
+                    cell.append([item])
+        return [frozenset(item) for item in cell]
+
+    # 过滤支持度低于阈值的项集
+    def CkLowSupportFilter(self, dataset, Ck):
+        CkCount = dict()
+        for data in dataset:
+            for cand in Ck:
+                if cand.issubset(data):
+                    if cand not in CkCount:
+                        CkCount[cand] = 1
+                    else:
+                        CkCount[cand] += 1
+
+        items = float(len(dataset))
+        returnList = []
+        supRata = dict()
+        # 过滤非频繁项集
+        for key in CkCount:
+            support = CkCount[key] / items
+            if support >= self.support:
+                returnList.insert(0, key)
+            supRata[key] = support
+        return returnList, supRata
+
+    def aprioriGeneration(self, Fk, k):  # 当候选项元素大于2时，合并时检测是否子项集满足频繁
+        returnList = []
+        lenFk = len(Fk)
+
+        for i in range(lenFk):
+            for j in range(i + 1, lenFk):
+                # 第k-2个项相同时，将两个集合合并
+                F1 = list(Fk[i])[:k - 2]
+                F2 = list(Fk[j])[:k - 2]
+                F1.sort()
+                F2.sort()
+                if F1 == F2:
+                    returnList.append(Fk[i] | Fk[j])
+        return returnList
+
+    """
+    产生强关联规则算法实现
+    基于Apriori算法，首先从一个频繁项集开始，接着创建一个规则列表，
+    其中规则右部只包含一个元素，然后对这些规则进行测试。
+    接下来合并所有的剩余规则列表来创建一个新的规则列表，
+    其中规则右部包含两个元素。这种方法称作分级法。
+    :param freq: 频繁项集
+    :param supRata: 频繁项集对应的支持度
+    :return: 强关联规则列表
+    """
+    def generateRules(self, freq, supRata):
+        strongRulesList = []
+        for i in range(1, len(freq)):
+            for freqSet in freq[i]:
+                H1 = [frozenset([item]) for item in freqSet]
+                # 只获取有两个或更多元素的集合
+                if i > 1:
+                    self.rulesFromReasonedItem(freqSet, H1, supRata, strongRulesList)
                 else:
-                    ssCnt[can] += 1
+                    self.calConf(freqSet, H1, supRata, strongRulesList)
+        return strongRulesList
 
-    retList = []
-    supportData = {}
-    for key in ssCnt:
-        if numItem == 0:
-            continue
-        support = ssCnt[key] / numItem
-        if support >= minSupport:
-            retList.insert(0, key)
-            supportData[key] = support
-    return retList, supportData  # 返回频繁k项集，相应支持度
+    def rulesFromReasonedItem(self, freqSet, H, supRata, strongRulesList):
+        """
+        H->出现在规则右部的元素列表
+        """
+        m = len(H[0])
+        if len(freqSet) > (m + 1):
+            Hmp1 = self.aprioriGeneration(H, m + 1)
+            Hmp1 = self.calConf(freqSet, Hmp1, supRata, strongRulesList)
+            if len(Hmp1) > 1:
+                self.rulesFromReasonedItem(freqSet, Hmp1, supRata, strongRulesList)
 
-
-def aprioriGen(Lk, k):  # create ck(k项集)
-    retList = []
-    lenLk = len(Lk)
-    for i in range(lenLk):
-        for j in range(i + 1, lenLk):
-            L1 = list(Lk[i])[:k - 2]
-            L2 = list(Lk[j])[:k - 2]
-            L1.sort()
-            L2.sort()  # 排序
-            if L1 == L2:  # 比较i,j前k-1个项若相同，和合并它俩
-                retList.append(Lk[i] | Lk[j])  # 加入新的k项集 | stanf for union
-    return retList # ck
-
-
-def apriori(dataSet, minSupport=0.5):
-    C1 = createC1(dataSet) # c1 = return map
-    # D = map(set, dataSet) # D = map
-    D = dataSet
-    L1, supportData = scanD(D, C1, minSupport)  # 利用k项集生成频繁k项集（即满足最小支持率的k项集）
-    L = [L1]  # L保存所有频繁项集
-
-    k = 2
-    while (len(L[k - 2]) > 0):  # 直到频繁k-1项集为空
-        Ck = aprioriGen(L[k - 2], k)  # 利用频繁k-1项集 生成k项集
-        Lk, supK = scanD(D, Ck, minSupport)
-        supportData.update(supK)  # 保存新的频繁项集与其支持度
-        L.append(Lk)  # 保存频繁k项集
-        k += 1
-    return L, supportData  # 返回所有频繁项集，与其相应的支持率
+    def calConf(self, freqSet, H, supRata, strongRulesList):  # 评估规则
+        prunedH = []
+        for reasonItem in H:
+            sup = supRata[freqSet]
+            conf = sup / supRata[freqSet - reasonItem]
+            lift = conf / supRata[reasonItem]
+            jaccard = sup / (supRata[freqSet - reasonItem] + supRata[reasonItem] - sup)
+            if conf >= self.confidence:
+                strongRulesList.append((freqSet - reasonItem, reasonItem, sup, conf, lift, jaccard))
+                prunedH.append(reasonItem)
+        return prunedH
 
 
-def calcConf(freqSet, H, supportData, brl, minConf=0.7):
-    prunedH = []
-    lift = []
-    file = open("generate_rules.txt","a",encoding = "utf-8")
-    for conseq in H:  # 后件中的每个元素
-        conf = supportData[freqSet] / supportData[freqSet - conseq]
-        if conf >= minConf:
-            file.write(str(freqSet - conseq)+"-->"+str(conseq)+" support:"+str(supportData[freqSet])+" conf:"+str(conf)+'\n')
-            brl.append((freqSet - conseq, conseq, supportData[freqSet], conf))  # 添加入规则集中
-            prunedH.append(conseq)  # 添加入被修剪过的H中
-    file.close()
-    return prunedH
+class OaklandCrimeStatistics():
+    def __init__(self):
+        # 结果文件路径
+        self.resultPath = '/Users/suxiangying/Documents/Data/results'
+
+        pass
+
+    def dataRead(self):
+
+        data2011 = pd.read_csv("/Users/suxiangying/Documents/Data/Oakland-Crime/records-for-2011.csv", encoding="utf-8")
+        data2012 = pd.read_csv("/Users/suxiangying/Documents/Data/Oakland-Crime/records-for-2012.csv", encoding="utf-8")
+        data2013 = pd.read_csv("/Users/suxiangying/Documents/Data/Oakland-Crime/records-for-2013.csv", encoding="utf-8")
+        data2014 = pd.read_csv("/Users/suxiangying/Documents/Data/Oakland-Crime/records-for-2014.csv", encoding="utf-8")
+        data2015 = pd.read_csv("/Users/suxiangying/Documents/Data/Oakland-Crime/records-for-2015.csv", encoding="utf-8")
+        data2016 = pd.read_csv("/Users/suxiangying/Documents/Data/Oakland-Crime/records-for-2016.csv", encoding="utf-8")
+
+        """
+        print("2011数据集有以下属性", data2011.columns)
+        print("2012数据集有以下属性", data2012.columns)
+        print("2013数据集有以下属性", data2013.columns)
+        print("2014数据集有以下属性", data2014.columns)
+        print("2015数据集有以下属性", data2015.columns)
+        print("2016数据集有以下属性", data2016.columns)
+        """
+
+        data2012.rename(columns={"Location 1": "Location"}, inplace=True)
+        data2013.rename(columns={"Location ": "Location"}, inplace=True)
+        data2014.rename(columns={"Location 1": "Location"}, inplace=True)
+
+        data2011temp = data2011[
+            ["Agency", "Location", "Area Id", "Beat", "Priority", "Incident Type Id", "Incident Type Description",
+             "Event Number"]]
+        data2012temp = data2012[
+            ["Agency", "Location", "Area Id", "Beat", "Priority", "Incident Type Id", "Incident Type Description",
+             "Event Number"]]
+        data2013temp = data2013[
+            ["Agency", "Location", "Area Id", "Beat", "Priority", "Incident Type Id", "Incident Type Description",
+             "Event Number"]]
+        data2014temp = data2014[
+            ["Agency", "Location", "Area Id", "Beat", "Priority", "Incident Type Id", "Incident Type Description",
+             "Event Number"]]
+        data2015temp = data2015[
+            ["Agency", "Location", "Area Id", "Beat", "Priority", "Incident Type Id", "Incident Type Description",
+             "Event Number"]]
+        data2016temp = data2016[
+            ["Agency", "Location", "Area Id", "Beat", "Priority", "Incident Type Id", "Incident Type Description",
+             "Event Number"]]
+
+        dataAll = pd.concat([data2011temp, data2012temp, data2013temp, data2014temp, data2015temp, data2016temp],
+                             axis=0)
+        print("综合数据集有以下属性", dataAll.columns)
+        dataAll = dataAll.dropna(how='any')
+
+        return dataAll.head(50000)
+        # return dataAll
+
+    def mining(self, feature_list):
+        outPath = self.resultPath
+        association = AssociationRules()
+
+        dataAll = self.dataRead()
+        rows = dataAll.values.tolist()
+
+        # 将数据转为数据字典存储
+
+        dataSet = []
+        featureNames = ["Agency", "Location", "Area Id", "Beat", "Priority", "Incident Type Id",
+                         "Incident Type Description", "Event Number"]
+        for dataLine in rows:
+            dataSet = []
+            for i, value in enumerate(dataLine):
+                if not value:
+                    dataSet.append((featureNames[i], 'NA'))
+                else:
+                    dataSet.append((featureNames[i], value))
+            dataSet.append(dataSet)
+
+        # 获取频繁项集
+        freqSet, supRata = association.apriori(dataSet)
+        supRataOut = sorted(supRata.items(), key=lambda d: d[1], reverse=True)
+        print("supRata ", supRata)
+        # 获取强关联规则列表
+        strongRulesList = association.generateRules(freqSet, supRata)
+        strongRulesList = sorted(strongRulesList, key=lambda x: x[3], reverse=True)
+        print("strongRulesList ", strongRulesList)
+
+        # 将频繁项集输出到结果文件
+        freqSetFile = open(os.path.join(outPath, 'Frequent.json'), 'w')
+        for (key, value) in supRataOut:
+            resultDict = {'set': None, 'sup': None}
+            setResult = list(key)
+            supResult = value
+            if supResult < support:
+                continue
+            resultDict['set'] = setResult
+            resultDict['sup'] = supResult
+            jsonStr = json.dumps(resultDict, ensure_ascii=False)
+            freqSetFile.write(jsonStr + '\n')
+        freqSetFile.close()
+
+        # 将关联规则输出到结果文件
+        rulesFile = open(os.path.join(outPath, 'rules.json'), 'w')
+        for result in strongRulesList:
+            resultDict = {'XSet': None, 'YSet': None, 'sup': None, 'conf': None, 'lift': None, 'jaccard': None}
+            XSet, YSet, sup, conf, lift, jaccard = result
+            resultDict['XSet'] = list(XSet)
+            resultDict['YSet'] = list(YSet)
+            resultDict['sup'] = sup
+            resultDict['conf'] = conf
+            resultDict['lift'] = lift
+            resultDict['jaccard'] = jaccard
+
+            jsonStr = json.dumps(resultDict, ensure_ascii=False)
+            rulesFile.write(jsonStr + '\n')
+        rulesFile.close()
 
 
-def rulesFromConseq(freqSet, H, supportData, brl, minConf=0.7):
-    m = len(H[0])  # H是一系列后件长度相同的规则，所以取H0的长度即可
-    if (len(freqSet) > m + 1):
-        Hmp1 = aprioriGen(H, m + 1)
-        Hmp1 = calcConf(freqSet, Hmp1, supportData, brl, minConf)
-        if (len(Hmp1) > 1):
-            rulesFromConseq(freqSet, Hmp1, supportData, brl, minConf)
-
-
-def generateRules(L, supportData, minConf=0.7):
-    bigRuleList = []  # 存储规则
-    for i in range(1, len(L)):
-        for freqSet in L[i]:
-            H1 = [frozenset([item]) for item in freqSet]
-            if (i > 1):
-                rulesFromConseq(freqSet, H1, supportData, bigRuleList, minConf)
-            else:
-                calcConf(freqSet, H1, supportData, bigRuleList, minConf)
-    return bigRuleList
-
-
-def lift_eval(rules, suppData): # lift evaluation
-    # lift(A, B) = P(A交B) / (P(A) * P(B)) = P(A) * P(B | A) / (P(A) * P(B)) = P(B | A) / P(B) = confidence(A— > B) / support(B) = confidence(B— > A) / support(A)
-    lift = []
-    for rule in rules:
-        freqSet_conseq = rule[0]
-        conseq = rule[1]
-        lift_val = float(rule[3]) / float(suppData[rule[1]])
-        lift.append([freqSet_conseq,conseq,lift_val])
-    return lift
-dataSet = loadDataSet()
-L, suppData = apriori(dataSet)
-print(L)
-rules = generateRules(L, suppData, minConf=0.5)
-print(rules)
-lifts = lift_eval(rules, suppData)
-print(lifts)
+if __name__ == "__main__":
+    OaklandCrimeStatistics().mining(PropertyList)
