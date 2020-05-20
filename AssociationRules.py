5f50a77c1bc1443f69e8b4283b99915d240ad8e6
周洋 <zhouyang081@ke.com>
2020-05-20 14:13:34 +0800

    算法修改

--- AssociationRules.py
+++ AssociationRules.py
@@ -6,126 +6,10 @@ from progressbar import *
 
 support = 0.1
 confidence = 0.5
-
 PropertyList = ['Priority', 'Location', 'Area Id', 'beat', 'Priority', 'Incident Type Id', 'Incident Type Descripe',
                  'Event Number']
 
-
-class AssociationRules():
-    def __init__(self):
-        self.support = support
-        self.confidence = confidence
-
-    # Apriori算法
-    def apriori(self, dataSet):
-        # 生成单元数候选项集
-        cell = self.cellGeneration(dataSet)
-        dataSet = [set(data) for data in dataSet]
-        F1, supRata = self.CkLowSupportFilter(dataSet, cell)
-        F = [F1]
-        k = 2
-        while len(F[k - 2]) > 0:
-            # 大于2时，合并检测
-            Ck = self.aprioriGeneration(F[k - 2], k)
-            Fk, support_k = self.CkLowSupportFilter(dataSet, Ck)
-            supRata.update(support_k)
-            F.append(Fk)
-            k += 1
-        return F, supRata
-
-    # 生成单元数候选项集
-    def cellGeneration(self, dataSet):
-        cell = []
-        progress = ProgressBar()
-        for data in progress(dataSet):
-            for item in data:
-                if [item] not in cell:
-                    cell.append([item])
-        return [frozenset(item) for item in cell]
-
-    # 过滤支持度低于阈值的项集
-    def CkLowSupportFilter(self, dataset, Ck):
-        CkCount = dict()
-        for data in dataset:
-            for cand in Ck:
-                if cand.issubset(data):
-                    if cand not in CkCount:
-                        CkCount[cand] = 1
-                    else:
-                        CkCount[cand] += 1
-
-        items = float(len(dataset))
-        returnList = []
-        supRata = dict()
-        # 过滤非频繁项集
-        for key in CkCount:
-            support = CkCount[key] / items
-            if support >= self.support:
-                returnList.insert(0, key)
-            supRata[key] = support
-        return returnList, supRata
-
-    def aprioriGeneration(self, Fk, k):  # 当候选项元素大于2时，合并时检测是否子项集满足频繁
-        returnList = []
-        lenFk = len(Fk)
-
-        for i in range(lenFk):
-            for j in range(i + 1, lenFk):
-                # 第k-2个项相同时，将两个集合合并
-                F1 = list(Fk[i])[:k - 2]
-                F2 = list(Fk[j])[:k - 2]
-                F1.sort()
-                F2.sort()
-                if F1 == F2:
-                    returnList.append(Fk[i] | Fk[j])
-        return returnList
-
-    """
-    产生强关联规则算法实现
-    基于Apriori算法，首先从一个频繁项集开始，接着创建一个规则列表，
-    其中规则右部只包含一个元素，然后对这些规则进行测试。
-    接下来合并所有的剩余规则列表来创建一个新的规则列表，
-    其中规则右部包含两个元素。这种方法称作分级法。
-    :param freq: 频繁项集
-    :param supRata: 频繁项集对应的支持度
-    :return: 强关联规则列表
-    """
-    def generateRules(self, freq, supRata):
-        strongRulesList = []
-        for i in range(1, len(freq)):
-            for freqSet in freq[i]:
-                H1 = [frozenset([item]) for item in freqSet]
-                # 只获取有两个或更多元素的集合
-                if i > 1:
-                    self.rulesFromReasonedItem(freqSet, H1, supRata, strongRulesList)
-                else:
-                    self.calConf(freqSet, H1, supRata, strongRulesList)
-        return strongRulesList
-
-    def rulesFromReasonedItem(self, freqSet, H, supRata, strongRulesList):
-        """
-        H->出现在规则右部的元素列表
-        """
-        m = len(H[0])
-        if len(freqSet) > (m + 1):
-            Hmp1 = self.aprioriGeneration(H, m + 1)
-            Hmp1 = self.calConf(freqSet, Hmp1, supRata, strongRulesList)
-            if len(Hmp1) > 1:
-                self.rulesFromReasonedItem(freqSet, Hmp1, supRata, strongRulesList)
-
-    def calConf(self, freqSet, H, supRata, strongRulesList):  # 评估规则
-        prunedH = []
-        for reasonItem in H:
-            sup = supRata[freqSet]
-            conf = sup / supRata[freqSet - reasonItem]
-            lift = conf / supRata[reasonItem]
-            jaccard = sup / (supRata[freqSet - reasonItem] + supRata[reasonItem] - sup)
-            if conf >= self.confidence:
-                strongRulesList.append((freqSet - reasonItem, reasonItem, sup, conf, lift, jaccard))
-                prunedH.append(reasonItem)
-        return prunedH
-
-
+#数据处理类
 class OaklandCrimeStatistics():
     def __init__(self):
         # 结果文件路径
@@ -242,6 +126,117 @@ class OaklandCrimeStatistics():
             rulesFile.write(jsonStr + '\n')
         rulesFile.close()
 
-
 if __name__ == "__main__":
     OaklandCrimeStatistics().mining(PropertyList)
+
+
+#关联规则算法类
+class AssociationRules():
+    def __init__(self):
+        self.support = support
+        self.confidence = confidence
+
+    # Apriori算法
+    def apriori(self, dataSet):
+        # 生成单元数候选项集
+        cell = self.cellGeneration(dataSet)
+        dataSet = [set(data) for data in dataSet]
+        F1, supRata = self.CkLowSupportFilter(dataSet, cell)
+        F = [F1]
+        k = 2
+        while len(F[k - 2]) > 0:
+            # 大于2时，合并检测
+            Ck = self.aprioriGeneration(F[k - 2], k)
+            Fk, support_k = self.CkLowSupportFilter(dataSet, Ck)
+            supRata.update(support_k)
+            F.append(Fk)
+            k += 1
+        return F, supRata
+
+    # 生成单元数候选项集
+    def cellGeneration(self, dataSet):
+        cell = []
+        progress = ProgressBar()
+        for data in progress(dataSet):
+            for item in data:
+                if [item] not in cell:
+                    cell.append([item])
+        return [frozenset(item) for item in cell]
+
+    # 过滤支持度低于阈值的项集
+    def CkLowSupportFilter(self, dataset, Ck):
+        CkCount = dict()
+        for data in dataset:
+            for cand in Ck:
+                if cand.issubset(data):
+                    if cand not in CkCount:
+                        CkCount[cand] = 1
+                    else:
+                        CkCount[cand] += 1
+
+        items = float(len(dataset))
+        returnList = []
+        supRata = dict()
+        # 过滤非频繁项集
+        for key in CkCount:
+            support = CkCount[key] / items
+            if support >= self.support:
+                returnList.insert(0, key)
+            supRata[key] = support
+        return returnList, supRata
+
+    def aprioriGeneration(self, Fk, k):  # 当候选项元素大于2时，合并时检测是否子项集满足频繁
+        returnList = []
+        lenFk = len(Fk)
+
+        for i in range(lenFk):
+            for j in range(i + 1, lenFk):
+                # 第k-2个项相同时，将两个集合合并
+                F1 = list(Fk[i])[:k - 2]
+                F2 = list(Fk[j])[:k - 2]
+                F1.sort()
+                F2.sort()
+                if F1 == F2:
+                    returnList.append(Fk[i] | Fk[j])
+        return returnList
+
+    """
+    产生强关联规则算法
+    :param freq: 频繁项集
+    :param supRata: 频繁项集对应的支持度
+    :return: 强关联规则列表
+    """
+    def generateRules(self, freq, supRata):
+        strongRulesList = []
+        for i in range(1, len(freq)):
+            for freqSet in freq[i]:
+                H1 = [frozenset([item]) for item in freqSet]
+                # 只获取有两个或更多元素的集合
+                if i > 1:
+                    self.rulesFromReasonedItem(freqSet, H1, supRata, strongRulesList)
+                else:
+                    self.calConf(freqSet, H1, supRata, strongRulesList)
+        return strongRulesList
+
+    def rulesFromReasonedItem(self, freqSet, H, supRata, strongRulesList):
+        """
+        H->出现在规则右部的元素列表
+        """
+        m = len(H[0])
+        if len(freqSet) > (m + 1):
+            Hmp1 = self.aprioriGeneration(H, m + 1)
+            Hmp1 = self.calConf(freqSet, Hmp1, supRata, strongRulesList)
+            if len(Hmp1) > 1:
+                self.rulesFromReasonedItem(freqSet, Hmp1, supRata, strongRulesList)
+
+    def calConf(self, freqSet, H, supRata, strongRulesList):  # 评估规则
+        prunedH = []
+        for reasonItem in H:
+            sup = supRata[freqSet]
+            conf = sup / supRata[freqSet - reasonItem]
+            lift = conf / supRata[reasonItem]
+            jaccard = sup / (supRata[freqSet - reasonItem] + supRata[reasonItem] - sup)
+            if conf >= self.confidence:
+                strongRulesList.append((freqSet - reasonItem, reasonItem, sup, conf, lift, jaccard))
+                prunedH.append(reasonItem)
+        return prunedH
